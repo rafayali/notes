d9ff456966fb63452c3ae2d0da5f1509fc040966
Rafay Ali <rufi1233@gmail.com>
2019-12-29 08:42:18 +0500

    Setup basic espresso and unit tests

--- app/src/androidTest/java/com/rafay/notes/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/rafay/notes/ExampleInstrumentedTest.kt
@@ -1,9 +1,9 @@
 package com.rafay.notes
 
-import androidx.test.InstrumentationRegistry
-import androidx.test.runner.AndroidJUnit4
-import org.junit.Assert.assertEquals
-
+import androidx.test.core.app.ApplicationProvider
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.filters.SmallTest
+import com.google.common.truth.Truth.assertThat
 import org.junit.Test
 import org.junit.runner.RunWith
 
@@ -13,11 +13,12 @@ import org.junit.runner.RunWith
  * See [testing documentation](http://d.android.com/tools/testing).
  */
 @RunWith(AndroidJUnit4::class)
+@SmallTest
 class ExampleInstrumentedTest {
     @Test
     fun useAppContext() {
         // Context of the app under test.
-        val appContext = InstrumentationRegistry.getTargetContext()
-        assertEquals("com.rafay.notes", appContext.packageName)
+        val appContext = ApplicationProvider.getApplicationContext<NotesApplication>()
+        assertThat("com.rafay.notes").matches(appContext.packageName)
     }
 }
--- app/src/test/java/com/rafay/notes/ExampleUnitTest.kt
+++ app/src/test/java/com/rafay/notes/ExampleUnitTest.kt
@@ -1,7 +1,7 @@
 package com.rafay.notes
 
-import org.junit.Assert.assertEquals
 import org.junit.Test
+import com.google.common.truth.Truth.assertThat
 
 /**
  * Example local unit test, which will execute on the development machine (host).
@@ -9,8 +9,9 @@ import org.junit.Test
  * See [testing documentation](http://d.android.com/tools/testing).
  */
 class ExampleUnitTest {
+
     @Test
     fun addition_isCorrect() {
-        assertEquals(4, 2 + 2)
+        assertThat(listOf("A","B")).hasSize(2)
     }
 }
