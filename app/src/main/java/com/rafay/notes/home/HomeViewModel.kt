eebeb9745cdb9f5de45f6e9c3d74a0e316acfa24
Rafay Ali <rufi1233@gmail.com>
2020-02-15 20:22:18 +0500

    Add shell script to decrypt google services

--- app/src/main/java/com/rafay/notes/home/HomeViewModel.kt
+++ app/src/main/java/com/rafay/notes/home/HomeViewModel.kt
@@ -1,31 +1,19 @@
 package com.rafay.notes.home
 
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.ViewModel
-import androidx.lifecycle.asLiveData
-import androidx.lifecycle.liveData
+import androidx.lifecycle.*
 import com.rafay.notes.common.Result
 import com.rafay.notes.db.dao.NotesDao
 import com.rafay.notes.db.entities.toNoteUiModel
 import com.rafay.notes.repository.NotesRemoteRepository
 import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.launch
+import timber.log.Timber
 
 /**
  * [ViewModel] for [HomeActivity].
  */
 class HomeViewModel(notesRemoteRepository: NotesRemoteRepository, notesDao: NotesDao) :
     ViewModel() {
-
-    /*@ExperimentalCoroutinesApi
-    val notes: LiveData<Result<List<NoteUiModel>>> = liveData {
-        emitSource(
-            notesRemoteRepository.all()
-                .onStart { Result.Loading }
-                .map {
-                    Result.Success(it.map { note -> note.toNoteUiModel() })
-                }.asLiveData()
-        )
-    }*/
 
     val notesLocal: LiveData<Result<List<NoteUiModel>>> = liveData {
         emitSource(
