1d5a631204dab38bbf5e884fffb62f1dcab48fa1
Rafay Ali <rufi1233@gmail.com>
2021-05-06 02:02:31 +0500

    fix: failing build due to incompatible moshi and room versions

--- app/src/main/java/com/rafay/notes/NotesApplication.kt
+++ app/src/main/java/com/rafay/notes/NotesApplication.kt
@@ -5,11 +5,9 @@ import androidx.work.Configuration
 import androidx.work.WorkManager
 import com.rafay.notes.work.KoinWorkerFactory
 import org.koin.android.ext.koin.androidContext
-import org.koin.core.component.KoinApiExtension
 import org.koin.core.context.startKoin
 import timber.log.Timber
 
-@KoinApiExtension
 @Suppress("unused")
 class NotesApplication : Application() {
 
@@ -20,6 +18,7 @@ class NotesApplication : Application() {
             Timber.plant(Timber.DebugTree())
         }
 
+        @Suppress("EXPERIMENTAL_API_USAGE")
         WorkManager.initialize(
             applicationContext,
             Configuration.Builder().setWorkerFactory(KoinWorkerFactory()).build()
